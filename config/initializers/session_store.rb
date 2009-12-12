7df862e58705aff571a0cba3360ff0bf25fca62b
Carl Tanner <carl@captproton.com>
2009-12-12 13:24:47 -0800

    New Heroku Suspenders app

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,8 +5,8 @@
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
 ActionController::Base.session = {
-  :session_key => "_CHANGEME_session",
-  :secret      => "CHANGESESSION"
+  :session_key => "_tour_calendar_session",
+  :secret      => "2e2d563dfb6c60bc503f84d7877e8e55"
 }
 
 # Use the database for sessions instead of the cookie-based default,
